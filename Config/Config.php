00a9724323016fca42964e3b058bf7796f39c34c
kjevingalindo <kevinjoelgalin18@gmail.com>
2022-02-03 20:04:36 -0500

    configuracion BD

---
+++ Config/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+const base_url = "http://localhost/FINALKEVIN/";
+const host = "localhost";
+const port = 33065;
+const user = "root";
+const pass = "";
+const db = "sistema2";
+const charset = "charset=utf8";
+
+?>
