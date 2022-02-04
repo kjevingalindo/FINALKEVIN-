e8edba93f8082df1c45ba0c151069a8a4137f244
kjevingalindo <kevinjoelgalin18@gmail.com>
2022-02-03 19:59:39 -0500

    vista vencidas

---
+++ Views/Vencidas/index.php
@@ -0,0 +1,23 @@
+<?php include "Views/Plantillas/header.php"; ?>
+<ol class="breadcrumb mb-4">
+    <li class="breadcrumb-item active">Tareas - Vencidas</li>
+</ol>
+<table class="table table-light" id="tblTodasVencidas">
+    <thead class="thead-dark">
+        <tr>
+            <th>Id</th>
+            <th>Titulo</th>
+            <th>Fecha</th>
+            <th>Fecha Vencimiento</th>
+            <th>Contenido</th>
+            <th>Prioridad</th>
+        </tr>
+    </thead>
+    <tbody>
+            
+    </tbody>
+    
+</table>
+
+
+<?php include "Views/Plantillas/footer.php"; ?>
