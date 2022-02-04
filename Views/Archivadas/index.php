aab8621060c80620ca8271771433871a2ed0a2c9
kjevingalindo <kevinjoelgalin18@gmail.com>
2022-02-03 19:52:17 -0500

    agregando vista archivadas

---
+++ Views/Archivadas/index.php
@@ -0,0 +1,21 @@
+<?php include "Views/Plantillas/header.php"; ?>
+<ol class="breadcrumb mb-4">
+    <li class="breadcrumb-item active">Tareas - Archivadas</li>
+</ol>
+
+<table class="table table-light" id="tblTodasArchivadas">
+    <thead class="thead-dark">
+        <tr>
+            <th>Id</th>
+            <th>Titulo</th>
+            <th>Fecha</th>
+            <th>Entrega</th>
+            <th>Contenido</th>
+            <th>Prioridad</th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>  
+    </tbody>
+</table>
+<?php include "Views/Plantillas/footer.php"; ?>
