b342de597dff7bfed85eb5aa2513dd2a8dc2f719
kjevingalindo <kevinjoelgalin18@gmail.com>
2022-02-03 19:53:39 -0500

    agregando vista pendientes

---
+++ Views/Pendientes/index.php
@@ -0,0 +1,22 @@
+<?php include "Views/Plantillas/header.php"; ?>
+<ol class="breadcrumb mb-4">
+    <li class="breadcrumb-item active">Tareas - Pendientes</li>
+</ol>
+<table class="table table-light" id="tblTodasPendientes">
+    <thead class="thead-dark">
+        <tr>
+            <th>Id</th>
+            <th>Titulo</th>
+            <th>Fecha</th>
+            <th>Fecha Vencimiento</th>
+            <th>Contenido</th>
+            <th>Prioridad</th>
+        </tr>
+    </thead>
+    <tbody>
+            
+    </tbody>
+</table>
+
+
+<?php include "Views/Plantillas/footer.php"; ?>
